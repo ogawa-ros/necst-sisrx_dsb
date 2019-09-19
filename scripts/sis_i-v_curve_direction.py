869eccf8023d8810b480917c8be789cee4ced9e5
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-09-19 17:35:16 +0900

    add sis-iv

--- scripts/sis_i-v_curve_direction.py
+++ scripts/sis_i-v_curve_direction.py
@@ -21,19 +21,9 @@ rospy.init_node(name)
 logger = core_controller.logger()
 sis = controller.sis()
 
-<<<<<<< HEAD
 date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
 file_name = name + '/' + date + '.necstdb'
-=======
-parser = argparse.ArgumentParser(description = 'measure SIS I-V curve only')
 
-parser.add_argument('save_name', type = str, help = 'set saving file name')
-
-args = parser.parse_args()
-
-date = datetime.datetime.today().strftime('%Y%m%d')
-file_name = '/home/exito/data/evaluation/' + date + '/sis_iv/%s'%(args.save_name)
->>>>>>> a1c25db8e57bd86a31ab7b9c75c07ee6fe8755e5
 print(file_name)
 
 sis_vgap = numpy.arange(-1.2, 1.2, 0.005)
