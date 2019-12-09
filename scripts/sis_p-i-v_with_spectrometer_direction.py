e007a72a23a321b9a148168538a739a2834efacb
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-12-09 21:10:07 +0900

    add ner direction

---
+++ scripts/sis_p-i-v_with_spectrometer_direction.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+name = 'sis_p-i-v_with_spectrometer_direction'
+
+import sys
+import datetime
+import rospy
+import time
+import std_msgs.msg
+import numpy
+import argparse
+import datetime
+
+sys.path.append("../../necst-core/scripts")
+
+import controller
+import core_controller
+
+rospy.init_node(name)
+
+logger = core_controller.logger()
+sis = controller.sis()
+
+date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+file_name = name + '/' + date + '.necstdb'
+
+print(file_name)
+
+sis_vgap = numpy.arange(0, 1.2, 0.005)
+sis.set_vgap(0.3)
+time.sleep(3)
+logger.start(file_name)
+for vgap in sis_vgap:
+    sis.set_vgap(vgap)
+    time.sleep(0.2)
+    continue
+logger.stop()
+sis.set_vgap(0)
---
+++ scripts/yfactor_with_v_by_spectorometer.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+name = 'yfactor_with_v_by_spectrometer'
+
+import sys
+import rospy
+import time
+import std_msgs.msg
+import numpy
+import argparse
+import datetime
+
+sys.path.append("../../necst-core/scripts")
+
+import controller
+import core_controller
+
+rospy.init_node(name)
+
+logger = core_controller.logger()
+sis = controller.sis()
+
+status = rospy.Publisher('/'+name+'/status', std_msgs.msg.String, queue_size=1)
+
+date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+file_name = name + '/' + date + '.necstdb'
+print(file_name)
+
+
+# set params.
+
+initial_voltage = 0.  # mV
+final_voltage   = 10. # mV
+step            = 0.1 # mV
+integ_time      = 1
+roop = int((final_voltage - initial_voltage) / step)
+
+
+logger.start(file_name)
+sis.set_v(float(initial_voltage))
+
+time.sleep(1)
+
+input('READY HOT MEASUREMENT? PRESS ENTER!!')
+status.publish("{0:4}".format("hot"))
+
+for i in range(roop+1):
+    v = initial_voltage + i*step
+    sis.set_v(float(v))
+    time.sleep(integ_time)
+
+sis.set_v(float(initial_voltage))
+
+input('READY COLD MEASUREMENT? PRESS ENTER!!')
+status.publish("{0:4}".format("cold"))
+
+for i in range(roop+1):
+    v = initial_voltage + i*step
+    sis.set_v(float(v))
+    time.sleep(integ_time)
+status.publish("{0:4}".format("hot"))
+time.sleep(0.1)
+logger.stop()
+
+sis.set_v(0)
+
+print('')
+print('FINISH!!')
+print('')
